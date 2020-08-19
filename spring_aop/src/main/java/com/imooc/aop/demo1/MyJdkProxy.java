cd9772742e17ab71071aa03c00a1dea8b738ac14
PHX_zwc <zhwchex@qq.com>
2020-08-19 23:12:31 +0800

    @note AOP JDK动态代理方式 20200819@home

--- spring_aop/src/main/java/com/imooc/aop/demo1/MyJdkProxy.java
+++ spring_aop/src/main/java/com/imooc/aop/demo1/MyJdkProxy.java
@@ -4,6 +4,13 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+
+/*@note
+Proxy.newProxyInstance(参数1，参数2，参数3);
+1.类的加载器，
+2.类实现的接口
+3.InvocationHandler接口，可以使类实现该接口，也可以使用匿名内部类的方式对该接口进行实现
+* */
 public class MyJdkProxy implements InvocationHandler{
     private UserDao userDao;
 
@@ -11,11 +18,13 @@ public class MyJdkProxy implements InvocationHandler{
         this.userDao = userDao;
     }
 
+    //加强类 @note
     public Object createProxy(){
         Object proxy = Proxy.newProxyInstance(userDao.getClass().getClassLoader(),userDao.getClass().getInterfaces(),this);
         return proxy;
     }
 
+    //advice， JDK动态代理的方法， 加强save方法 @note
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         if("save".equals(method.getName())){
             System.out.println("权限校验...");
